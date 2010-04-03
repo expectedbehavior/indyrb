e4e894c7b7584e7342d7345c21347c0697c439d5
Anthony Panozzo <panozzaj@gmail.com>
2010-04-03 17:35:26 -0400

    Must have not run the migration previously.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100403201504) do
+ActiveRecord::Schema.define(:version => 20100403211105) do
 
   create_table "source_commits", :force => true do |t|
     t.text     "url"
