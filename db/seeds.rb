fa95bb8bc46eb4bb3d56202616b2d7d5e026fd79
Dave Jones <djones@nframe.com>
2010-04-03 16:31:04 -0400

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,23 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#   
+#
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+users = [
+  { :name => 'Dave Jones',     :github => 'unixmonkey',   :twitter => 'unixmonkey',   :meetup => '4656402', :email => 'unixmonkey1@gmail.com'  },
+  { :name => 'Miles Sterrett', :github => 'mileszs',      :twitter => 'mileszs',      :meetup => '7552573', :email => 'miles.sterrett@gmail.com' },
+  { :name => 'Matt Gordon',    :github => 'systematic',   :twitter => 'esquivalient', :meetup => '9648110', :email => '' },
+  { :name => 'Joel Meador',    :github => 'janxious',     :twitter => 'joelmeador',   :meetup => '767579',  :email => '' },
+  { :name => 'Nolan Eakins',   :github => 'sneakin',      :twitter => 'sneakin',      :meetup => '1636703', :email => 'nolan@eakins.net' }
+]
+
+users.each do |user|
+  User.find_or_create_by_name(:name    => user[:name],
+                              :github  => user[:github],
+                              :twitter => user[:twitter],
+                              :meetup  => user[:meetup],
+                              :email   => user[:email])
+end
+puts "#{User.count} Users Seeded"
