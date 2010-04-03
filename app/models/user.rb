0e248f443cb3019f9c3038b33ae38829c400ab0e
Dave Jones <djones@nframe.com>
2010-04-03 17:03:22 -0400

    Add _url methods for github/meetup/twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
     config.validate_email_field false
     config.validate_password_field false
   end
+
+  def github_url
+    github.present? ? "http://github.com/#{github}" : nil
+  end
+
+  def meetup_url
+    meetup.present? ? "http://meetup.com/members/#{meetup}" : nil
+  end
+
+  def twitter_url
+    twitter.present? ? "http://twitter.com/#{twitter}" : nil
+  end
+
 end
