aa8b6d13c01f892c1979b936112f2dc1894d64e2
Matthew Hassfurder <matthew@fastestforward.com>
2010-06-26 18:51:14 -0400

    Annotate models and fixtures.

--- test/unit/source_commit_test.rb
+++ test/unit/source_commit_test.rb
@@ -6,3 +6,19 @@ class SourceCommitTest < ActiveSupport::TestCase
     assert true
   end
 end
+
+# == Schema Information
+#
+# Table name: source_commits
+#
+#  id             :integer(4)      not null, primary key
+#  url            :text
+#  project_name   :string(255)
+#  message        :text
+#  committed_date :datetime
+#  created_at     :datetime
+#  updated_at     :datetime
+#  github_id      :string(255)
+#  user_id        :integer(4)
+#
+
